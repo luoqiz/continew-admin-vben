d6ca16d3ec1951e33859a5928730eea1f34c4df5
luoqiz <luoqiangzheng@gmail.com>
2025-07-21 20:04:09 +0800

    feat: 合并vben5.5.8

--- apps/web-ele/components.d.ts
+++ apps/web-ele/components.d.ts
@@ -18,4 +18,7 @@ declare module 'vue' {
     RouterView: typeof import('vue-router')['RouterView']
     Src: typeof import('./src/components/dict/src/index.vue')['default']
   }
+  export interface GlobalDirectives {
+    vLoading: typeof import('element-plus/es')['ElLoadingDirective']
+  }
 }
