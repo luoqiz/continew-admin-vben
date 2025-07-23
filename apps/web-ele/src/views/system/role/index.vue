f3e6c9d172c63e3db84f86f400305ef09cd51c31
luoqiz <851092732@qq.com>
2025-07-23 17:59:15 +0800

    feat: 适配角色管理

---
+++ apps/web-ele/src/views/system/role/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+import { ColPage } from '@vben/common-ui';
+
+import RoleList from './left/RoleList.vue';
+import Details from './right/Details.vue';
+
+defineOptions({ name: 'SystemDict' });
+const props = reactive({
+  leftCollapsedWidth: 5,
+  leftCollapsible: true,
+  leftMaxWidth: 50,
+  leftMinWidth: 20,
+  leftWidth: 50,
+  resizable: true,
+  rightWidth: 50,
+  splitHandle: false,
+  splitLine: false,
+});
+</script>
+
+<template>
+  <ColPage auto-content-height v-bind="props">
+    <template #left>
+      <RoleList />
+    </template>
+    <Details />
+  </ColPage>
+</template>
+
+<style scoped lang="scss"></style>
